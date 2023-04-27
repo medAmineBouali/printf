1db15ba72517e6c55716c9b349c1945c1bae2468
amine1 <medaminebouali1@gmail.com>
2023-04-27 22:26:19 +0000

    task

--- lists.h
+++ lists.h
@@ -1,6 +1,5 @@
 #ifndef LISTS_H
 #define LISTS_H
-size_t print_list(const list_t *h);
 /**
 * struct list_s - singly linked list
 * @str: string - (malloc'ed string)
@@ -14,4 +13,5 @@ typedef struct list_s
 	        unsigned int len;
 		struct list_s *next;
 } list_t;
+size_t print_list(const list_t *h);
 #endif
